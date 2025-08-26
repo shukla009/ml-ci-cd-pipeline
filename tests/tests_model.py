6e248496d029fd3c8b3408052e8a634009e3a227
shukla009 <shuklashivank009@duck.com>
2025-08-26 11:23:42 +0530

    Sixth Commit

--- tests/tests_model.py
+++ tests/tests_model.py
@@ -1,10 +1,11 @@
 import unittest
 import joblib
-from sklearn.ensemble import RandomForesrClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 
 class TestModelTraining(unittest.TestCase):
 	def test_model_training(self):
-		model = joblib.load('model/iris_model.pkl')
+		model = joblib.load('model/iris.pkl')
 		self.assertIsInstance(model, RandomForestClassifier)
 		self.assertGreaterEqual(len(model.feature_importances_), 4)
 
